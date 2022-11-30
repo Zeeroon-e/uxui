aee22ca175e10d0cb24fa7c3bfbfee9729f9e748
Zeeroon-e <chia.kmohamad@hotmail.com>
2022-11-30 19:49:42 +0100

    default

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './header.css'
+function Header() {
+  return (
+    <div className='nav'>
+      <ul>
+        <li> h</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Header
